0a324349edc8e109af34bf20eacfa35a8ae96ca9
Niang Mohamed Lamine <106320478+lamine2002@users.noreply.github.com>
2024-06-25 16:24:28 +0000

    Implementation de Home.vue

---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<script setup>
+import axios from 'axios';
+import {ref} from "vue";
+
+const questions = ref([]);
+
+axios.get('http://127.0.0.1:8000/api/questions')
+  .then(response => {
+    console.log(response.data.questions);
+    questions.value = response.data.questions;
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+</script>
+
+<template>
+  <div class="container mx-auto px-4">
+
+      <div v-for="question in questions" :key="question.id" class="border p-4 rounded-md mt-4">
+        <h2 class="text-xl font-bold mb-2">{{ question.title }}</h2>
+        <p class="text-gray-700">{{ question.body }}</p>
+        <div class="mt-2">
+          <span v-for="tag in question.tags" :key="tag.id" class="mr-1 inline-block bg-blue-200 text-blue-800 p-2 rounded-full text-sm font-semibold tracking-wide">{{ tag.name }}</span>
+        </div>
+      </div>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
