372d54cc96c59f746d0d1daa0ee684ee2c1b3c33
Niang Mohamed Lamine <106320478+lamine2002@users.noreply.github.com>
2024-06-26 22:01:10 +0000

    Ajout de createQuestion fonction

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,9 @@
 <script setup>
 import axios from 'axios';
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
 import Sidebar from "./Sidebar.vue";
+import {getUserConnected} from "../userConnected.js";
+import router from "../router.js";
 
 const questions = ref([]);
 
@@ -14,13 +16,28 @@ axios.get('http://127.0.0.1:8000/api/questions')
     console.log(error);
   });
 
+const user = ref(null);
+
+onMounted(async () => {
+  user.value = await getUserConnected();
+  console.log(user.value);
+});
+const createQuestion = () => {
+  // verifier si l'utilisateur est connecte
+  if (user.value === null) {
+    alert('Vous devez vous connecter pour poser une question');
+    return;
+  }
+  // rediriger vers la page de creation de question
+  router.push('/question/create');
+}
 </script>
 
 <template>
 <!--  Mettre le titre Question et a cote ajoute un bouton poser une question qui est un lien situe tout a droite en utilisant les flex et justify-content-->
   <div class="flex justify-between items-center">
     <h1 class="text-4xl font-bold">Questions</h1>
-    <router-link to="/question/create" class="bg-blue-500 hover:bg-blue-700 text-white  py-2 px-4 rounded-lg">Une question ?</router-link>
+    <router-link @click="createQuestion" to="" class="bg-blue-500 hover:bg-blue-700 text-white  py-2 px-4 rounded-lg">Une question ?</router-link>
   </div>
 
   <div class="container mx-auto px-4">
