ee27ff4cc6cfffb84a96030a98bd978a216524ae
Niang Mohamed Lamine <106320478+lamine2002@users.noreply.github.com>
2024-06-28 19:13:30 +0000

    Ajout de svg pour les questions avec reponse valide

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -64,8 +64,6 @@ const sortedQuestions = computed(() => {
 });
 </script>
 
-
-
 <template>
   <div class="flex justify-between items-center">
     <h1 class="text-4xl font-bold">Questions</h1>
@@ -96,27 +94,82 @@ const sortedQuestions = computed(() => {
           <p class="text-gray-700">{{ question.body }}</p>
           <div class="mt-2">
             <span v-for="tag in question.tags" :key="tag.id" class="mr-1 inline-block bg-blue-100 text-blue-800 p-2 rounded-xl text-sm font-semibold tracking-wide">{{ tag.name }}</span>
-        </div>
-          <p v-if="question.answers.length === 1" class="question-with-response text-xs text-gray-500 mt-2  inline-block p-1 rounded-lg">{{'1 Réponse' }}</p>
-          <p v-if="question.answers.length > 1" class="question-with-response text-xs text-gray-500 mt-2 inline-block p-1 rounded-lg">{{question.answers.length + ' Réponses' }}</p>
-          <p v-if="question.answers.length <= 0" class="text-xs text-gray-500 mt-2 inline-block p-1 rounded-lg">{{'0 Réponses' }}</p>
+          </div>
+          <p
+              v-if="question.answers.length === 1"
+              :class="['question-with-response', { 'validated-answer': question.answers.some(answer => answer.is_validated) }]"
+              class="text-xs text-gray-500 mt-2 inline-block p-1 rounded-lg"
+          >
+            1 Réponse
+
+            <span v-if="question.answers.some(answer => answer.is_validated)" class="inline-block pl-1">
+              <svg fill="#fff" height="20px" width="20px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                   viewBox="0 0 496.124 496.124" xml:space="preserve">
+                <g>
+                  <path d="M420.001,219.7c3.666,14.313,5.811,29.238,5.811,44.676c0,99.828-81.199,181.024-181.022,181.024
+                    c-99.821,0-181.039-81.195-181.039-181.024c0-99.824,81.218-181.041,181.039-181.041c25.442,0,49.633,5.338,71.623,14.848
+                    l26.912-43.26c-29.949-14.15-63.27-22.307-98.535-22.307c-127.793,0-231.764,103.971-231.764,231.76
+                    c0,127.779,103.971,231.748,231.764,231.748c127.774,0,231.743-103.969,231.743-231.748c0-35.516-8.271-69.062-22.599-99.178
+                    L420.001,219.7z"/>
+                  <path d="M463.159,6.385c-19.811-12.32-45.882-6.279-58.217,13.533L236.88,289.985l-56.299-80.289
+                    c-13.389-19.104-39.775-23.729-58.879-10.338c-19.1,13.408-23.742,39.756-10.336,58.877l93.002,132.633
+                    c7.93,11.291,20.855,17.996,34.609,17.996c0.379,0,0.777,0,1.157-0.018c14.181-0.396,27.229-7.871,34.72-19.914L476.702,64.6
+                    C489.05,44.766,482.972,18.713,463.159,6.385z"/>
+                </g>
+              </svg>
+            </span>
+          </p>
+          <p
+              v-if="question.answers.length > 1"
+              :class="['question-with-response', { 'validated-answer': question.answers.some(answer => answer.is_validated) }]"
+              class="text-xs text-gray-500 mt-2 inline-block p-1 rounded-lg"
+          >
+            <span>
+              {{ question.answers.length + ' Réponses ' }}
+            </span>
+            <span v-if="question.answers.some(answer => answer.is_validated)" class="inline-block pl-1">
+              <svg fill="#fff" height="20px" width="20px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                   viewBox="0 0 496.124 496.124" xml:space="preserve">
+                <g>
+                  <path d="M420.001,219.7c3.666,14.313,5.811,29.238,5.811,44.676c0,99.828-81.199,181.024-181.022,181.024
+                    c-99.821,0-181.039-81.195-181.039-181.024c0-99.824,81.218-181.041,181.039-181.041c25.442,0,49.633,5.338,71.623,14.848
+                    l26.912-43.26c-29.949-14.15-63.27-22.307-98.535-22.307c-127.793,0-231.764,103.971-231.764,231.76
+                    c0,127.779,103.971,231.748,231.764,231.748c127.774,0,231.743-103.969,231.743-231.748c0-35.516-8.271-69.062-22.599-99.178
+                    L420.001,219.7z"/>
+                  <path d="M463.159,6.385c-19.811-12.32-45.882-6.279-58.217,13.533L236.88,289.985l-56.299-80.289
+                    c-13.389-19.104-39.775-23.729-58.879-10.338c-19.1,13.408-23.742,39.756-10.336,58.877l93.002,132.633
+                    c7.93,11.291,20.855,17.996,34.609,17.996c0.379,0,0.777,0,1.157-0.018c14.181-0.396,27.229-7.871,34.72-19.914L476.702,64.6
+                    C489.05,44.766,482.972,18.713,463.159,6.385z"/>
+                </g>
+              </svg>
+            </span>
+          </p>
+
+
+          <p
+              v-if="question.answers.length <= 0"
+              class="text-xs text-gray-500 mt-2 inline-block p-1 rounded-lg"
+          >
+            0 Réponses
+          </p>
         </div>
 
-<!--        l'utilisateur qui a pose la question -->
+        <!-- l'utilisateur qui a posé la question -->
         <div class="">
-          <p class="text-gray-700">{{'Posée par ' + question.user.name }} {{ timeAgo(question.created_at) }}</p>
+          <p class="text-gray-700">{{ 'Posée par ' + question.user.name }} {{ timeAgo(question.created_at) }}</p>
         </div>
       </router-link>
     </div>
   </div>
 </template>
 
-
-
 <style scoped>
-
 .question-with-response {
   border: 1.4px solid #0ce12e;
 }
 
+.validated-answer {
+  background-color: green;
+  color: white;
+}
 </style>
